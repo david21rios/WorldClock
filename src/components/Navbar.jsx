cfe0607e7491172f3b4310f99388b75311c10445
wilsonriosv <wilsonriosv@gmail.com>
2024-07-26 17:25:44 -0500

    Ajustes en login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,52 +4,52 @@ import {useContext }from 'react';
 import { AuthContext }from './AuthContext';
 
 const Navbar = () => {
-    const { user } = useContext(AuthContext)
+    const { user } = useContext(AuthContext);
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <Link className="navbar-brand" to="/">Navbar</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/about">About</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/contact">Contact</a>
-                    </li>
-                    <li className="nav-item"></li>
-                    <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Cuenta
-                    </a>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a className="dropdown-item" href="/login">Login</a></li>
-                        <li><a className="dropdown-item" href="/register">Register</a></li>
-                    </ul>
-                    </li>
-                    {user && (
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/user-profile">
-                                proyectos
-                            </Link>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                    )}
-                </ul>
-                <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                <p>{user ? user.email : 'Usuario'}</p>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/contact">Contact</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Cuenta
+                            </Link>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><Link className="dropdown-item" to="/login">Login</Link></li>
+                                <li><Link className="dropdown-item" to="/register">Register</Link></li>
+                            </ul>
+                        </li>
+                        {user && (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/user-profile">
+                                    Proyectos
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <p>{user ? user.email : 'Usuario'}</p>
                 </div>
             </div>
         </nav>
     );
 };
+
 export default Navbar;
